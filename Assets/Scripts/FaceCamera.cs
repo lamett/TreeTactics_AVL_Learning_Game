a41dd53442c24e7858047753c9094fb4f5d24994
Annabell K <akiessle@hs-mittweida.de>
2024-11-09 16:45:26 +0100

    node face kamera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour 
+{
+
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(mainCamera.transform);
+        transform.Rotate(0,180,0);
+    }
+ 
+}
