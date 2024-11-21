2d5641ef6283bc62e2ab665602e76eef71d75e09
DieterBowlen <newtunehd@gmail.com>
2024-11-21 14:35:04 +0100

    21.11.24 Update
    Rotationsbuttons Offset

--- Assets/Scripts/RotationButtons.cs
+++ Assets/Scripts/RotationButtons.cs
@@ -5,55 +5,79 @@ using UnityEngine.EventSystems;
 
 public class RotationButtons : MonoBehaviour
 {
-
     private AVLOperations currentNode;
 
+    
+    public GameObject panel; 
+    public Canvas canvas; 
+    public Vector3 panelOffset;
+
+   
     public void SetNode(AVLOperations newNode)
     {
         currentNode = newNode;
     }
-            
+    
     public void Left()
     {
         currentNode.RotateLeft();
-        gameObject.SetActive(false);
+        panel.SetActive(false); 
     }
-
-
+   
     public void Right()
     {
         currentNode.RotateRight();
-        gameObject.SetActive(false);
+        panel.SetActive(false); 
     }
-
     private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-            {
+    {        
+        if (Input.GetMouseButtonDown(0))
+        {            
             if (EventSystem.current.IsPointerOverGameObject())
             {
-                Debug.Log("");
+              
                 return;
             }
+           
             Ray ballRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ballRay, out RaycastHit ballHit, 100))
-            {
-                
-
-                if (!ballHit.collider.gameObject.TryGetComponent<AVLNode>(out AVLNode ball))
-                {
-                    Debug.Log(ballHit.collider.gameObject.name);
-                    gameObject.SetActive(false);
+            if (Physics.Raycast(ballRay, out RaycastHit ballHit, 100))
+            {               
+                if (ballHit.collider.gameObject.TryGetComponent<AVLNode>(out AVLNode ball))
+                {                                        
+                    PositionPanel(ballHit.collider.gameObject);
+                    
+                    SetNode(ball.GetComponent<AVLOperations>());
+                    
+                    panel.SetActive(true);
                 }
-            }else
-            {
-                Debug.Log("");
-                gameObject.SetActive(false);
-                return;
+                else
+                {
+                    Debug.Log($"Kein AVLNode: {ballHit.collider.gameObject.name}");
+                    panel.SetActive(false); 
+                }
             }
-                
-                
+            else
+            {                
+                panel.SetActive(false); 
             }
+        }
     }
 
+    private void PositionPanel(GameObject ball)
+    {      
+        Vector3 worldPosition = ball.transform.position;
+        
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);        
+        
+        Vector3 newPosition = screenPosition + panelOffset;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.GetComponent<RectTransform>(),
+            newPosition,
+            canvas.worldCamera,
+            out Vector2 canvasPosition
+        );
+        panel.GetComponent<RectTransform>().anchoredPosition = canvasPosition;
+    }
 }
+
