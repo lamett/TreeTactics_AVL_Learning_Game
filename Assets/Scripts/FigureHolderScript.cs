ee2e40218496085fa150dd76f37dfc5844539826
Firebro <temy5210@gmx.de>
2024-12-12 16:37:53 +0100

    added player/enemy figures

--- Assets/Scripts/FigureHolderScript.cs
+++ Assets/Scripts/FigureHolderScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FigureHolderScript : MonoBehaviour
 {
-    public GameObject Figur;
+    public GameObject Figur1;
+    public GameObject Figur2;
+    public GameObject Figur3;
     public ArmBehaviour Arm;
 
     List<GameObject> allFigures = new List<GameObject>();
     public void init(int num)
     {
+        GameObject[] figure_arr = new GameObject[3];
+        figure_arr[0] = Figur1;
+        figure_arr[1] = Figur2;
+        figure_arr[2] = Figur3;
         for (int i = 0; i < num; i++)
         {
-            var figur = Instantiate(Figur, gameObject.transform);
+            var figur = Instantiate(figure_arr[(i % 3)], gameObject.transform);
             figur.transform.localPosition = new Vector3(0, 0, i);
             figur.GetComponent<Rigidbody>().isKinematic = true;
             allFigures.Add(figur);
